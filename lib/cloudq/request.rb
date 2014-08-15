e4ccc3deb652aac7655865d6cc681c1196da94af
Kevin Stewart <kevin@jackrussellsoftware.com>
2014-08-15 13:58:34 -0400

    added timeout to Request.get to accomodate new version of cloudq; adjusted timeout of Consume.get to 300 from 20000 because the timeout is specified in seconds and 300 should be sufficient to accomodate the new version of cloudq

--- lib/cloudq/request.rb
+++ lib/cloudq/request.rb
@@ -1,13 +1,13 @@
 module Cloudq
   class Request < Base
     def job
-      get 
+      get
     end
 
   private
 
     def get
-      resp = RestClient.get url
+      resp = RestClient::Request.execute(:method => :get, :url => url, :timeout => 300, :open_timeout => 300)
       if resp.code == 200
         result = JSON.parse(resp)
         return nil if result['status'] == 'empty'
